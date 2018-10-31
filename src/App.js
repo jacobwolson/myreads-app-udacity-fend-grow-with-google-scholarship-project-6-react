1131ad47f2c1dd4e8fe9f1c0aa8843ff5d84ffa6
Jacob Olson <jwolson2@gmail.com>
2018-10-31 05:59:12 -0700

    Rename 'Bookshelf' component; change name to 'ListBooks'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import Bookshelf from './Bookshelf'
+import ListBooks from './ListBooks'
 import Search from './Search'
 import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class App extends Component {
+  placeBook = () => {
+
+  }
+
   render() {
     return(
-      <div>
+      <div class="app">
         <Route
           exact path="/"
-          component={Bookshelf}
+          component={ListBooks}
         />
         <Route 
           path="/search" 
