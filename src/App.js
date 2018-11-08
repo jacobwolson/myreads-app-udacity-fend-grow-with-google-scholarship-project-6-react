33a4d340c3a7d8dac163cf0275a76d563d527695
Jacob Olson <jwolson2@gmail.com>
2018-11-08 12:27:19 -0800

    Continue refactoring updateShelf method

--- src/App.js
+++ src/App.js
@@ -20,23 +20,11 @@ class App extends Component {
   /* Consulted for writing updateShelf method:
     */
   updateShelf = (newShelf, book) => {
-    /*  Option A: If book in question is already in `allBooksInPlay` state array then book in question will already
-    have a shelf property set -- and will display in both the listBooks component and in search results. In 
-    this case, run the `if` statemtent. 
-        Option B: Otherwise, if book in question does not have an existing shelf property -- 
-    which means it will only currently be displaying as a search result -- run the `else` statement. */
-    if (this.state.allBooksInPlay.filter(eachBook => eachBook.id === book.id) !== 0) {
-      const updatedAllBooksInPlayArray = this.state.allBooksInPlay.map(eachBook => eachBook.id === book.id ? eachBook = book : eachBook)
-      // Update value of the shelf property on, or add a shelf property and concomitant value to, the selected book.
-      book.shelf = newShelf
-      // Option A: set state with updated book object.
-      this.setState({allBooksInPlay: updatedAllBooksInPlayArray})
-    } else {
-      // Option B: set state, adding new book with newly added `shelf` property to state.
-      this.setState({allBooksInPlay: this.state.allBooksInPlay.concat(book)})
-    }
-    /* Update API with newly shelved book so that if we refresh or for whatever reason make new 
-    `BooksAPI.getAll()` call, app will still be in same state */
+    const updatedAllBooksInPlayArray = this.state.allBooksInPlay.map(eachBook => eachBook.id === book.id ? eachBook = book : eachBook)
+    // Update value of the shelf property on, or add a shelf property and concomitant value to, the selected book.
+    book.shelf = newShelf
+    // Set state with updated book object.
+    this.setState({allBooksInPlay: updatedAllBooksInPlayArray})
     BooksAPI.update(book, newShelf)
   }
 
